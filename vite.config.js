3032526e8cbc03c2910ce0fd30bd7af4be039417
Aditya <adigupta.951@gmail.com>
2025-03-31 09:07:07 +0530

    Updated project

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  
+  build: {
+    minify: 'terser', // Minifies JS with Terser
+    terserOptions: {
+      compress: { drop_console: true }, // Removes console logs
+    },
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'public, max-age=31536000, immutable',
+    },
+  },
 })
